30cb8fba78d0e640d45473586fe42fc2afd32a22
Rushikeshpalaskar2220 <rushikeshpalaskar467@gmail.com>
2023-04-15 22:46:29 +0530

    firstcommit

--- src/Componets/Map.js
+++ src/Componets/Map.js
@@ -37,7 +37,7 @@ const Contact = () => {
             <div className="contact">
               <li> Behind Jijamata Garden,  412210</li>
               <li>Jijamata Society Shirur</li>
-              <li>Maharashtra-412210 India</li>
+              <li>Maharashtra-412210 </li>
               <li>Call/Whatsap:-9822380705 / 9697975599</li>
             </div>
           </div>
